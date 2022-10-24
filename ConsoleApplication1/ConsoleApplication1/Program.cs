4989e75d2e1081dc478bde6a8c2f02257d5142fe
SHOUKAT <shoukatrahman2001@gmail.com>
2022-10-24 04:21:23 -0700

    name print

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main()
+        {
+            string Name;
+            Console.WriteLine("First enter your name ");
+            Name = Console.ReadLine();
+            Console.WriteLine("My Name is " + Name);          // String concatination....
+            Console.ReadLine();
+        }
+    }
+}
